ed07dc31044881cdbebeb5b58799839f2b706cf2
SungHoHong2 <maverickjin88@gmail.com>
2017-10-19 18:07:45 -0700

    add

--- INFINIBAND/IMB-Experiments/test/IMB_pingpong.c
+++ INFINIBAND/IMB-Experiments/test/IMB_pingpong.c
@@ -272,7 +272,7 @@ Output variables:
   	}
 
 	std_mean /=ITERATIONS->n_sample;
-	// std_mean = std_mean*pow(10,6)/2;
+	std_mean = std_mean*pow(10,6)/2;
 
 
 	// for(i=0;i<ITERATIONS->n_sample;i++){
