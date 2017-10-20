8a60522e88ce3479833f093f69b78e57a5c41577
SungHoHong2 <maverickjin88@gmail.com>
2017-10-19 18:07:18 -0700

    add

--- INFINIBAND/IMB-Experiments/test/IMB_pingpong.c
+++ INFINIBAND/IMB-Experiments/test/IMB_pingpong.c
@@ -272,15 +272,15 @@ Output variables:
   	}
 
 	std_mean /=ITERATIONS->n_sample;
-	std_mean = std_mean*pow(10,6)/2;
+	// std_mean = std_mean*pow(10,6)/2;
 
 
-	for(i=0;i<ITERATIONS->n_sample;i++){
-		std_real += pow(((std_array[0][i]*pow(10,6))-std_mean),2);
-	}
+	// for(i=0;i<ITERATIONS->n_sample;i++){
+	//	std_real += pow(((std_array[0][i]*pow(10,6))-std_mean),2);
+	// }
 
-	std_real/=ITERATIONS->n_sample;
-	std_real = pow(std_real,0.5);
+	// std_real/=ITERATIONS->n_sample;
+	// std_real = pow(std_real,0.5);
 
 	// printf("%s: n_sample: %d  total: %f, test_std: %f  std_mean: %f\n", debug_array, ITERATIONS->n_sample, (*time)*pow(10,6)/2, (test_std)*pow(10,6)/2, std_mean);
 	printf("%d-%s: n_sample: %d  avg: %f, std_mean: %f std_real: %f\n",z, debug_array, ITERATIONS->n_sample, (*time)*pow(10,6)/2, std_mean, std_real);
