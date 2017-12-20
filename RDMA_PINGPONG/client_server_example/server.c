f2e8f308e276d40db92462fe93c3d09bbf0980b9
SungHoHong2 <maverickjin88@gmail.com>
2017-12-20 12:53:21 -0700

    update RDMA experiments

--- RDMA_PINGPONG/client_server_example/server.c
+++ RDMA_PINGPONG/client_server_example/server.c
@@ -81,7 +81,8 @@ int main(int argc, char **argv)
 
     // where it actually listens
     if (on_event(&event_copy)){
-        printf("howdy chara\n");
+        // printf("howdy chara\n");
+        break;
     }
 
   }
