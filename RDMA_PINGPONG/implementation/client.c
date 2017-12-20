005e7f6634c42866acd8c0266426e70b8ad8929c
SungHoHong2 <maverickjin88@gmail.com>
2017-12-20 13:59:42 -0700

    update RDMA experiments

--- RDMA_PINGPONG/implementation/client.c
+++ RDMA_PINGPONG/implementation/client.c
@@ -4,12 +4,12 @@
 #include <unistd.h>
 #include <rdma/rdma_cma.h>
 
-#define TEST_NZ(x) do { if ( (x)) die("error: " #x " failed (returned non-zero)." ); } while (0)
-#define TEST_Z(x)  do { if (!(x)) die("error: " #x " failed (returned zero/null)."); } while (0)
+// #define TEST_NZ(x) do { if ( (x)) die("error: " #x " failed (returned non-zero)." ); } while (0)
+// #define TEST_Z(x)  do { if (!(x)) die("error: " #x " failed (returned zero/null)."); } while (0)
 
 
-static void die(const char *reason);
-static int on_event(struct rdma_cm_event *event);
+// static void die(const char *reason);
+// static int on_event(struct rdma_cm_event *event);
 
 
 int main(){
