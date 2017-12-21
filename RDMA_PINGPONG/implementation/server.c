ee71606f88da2fb4eef670739bd0b2bab5ee31a9
SungHoHong2 <maverickjin88@gmail.com>
2017-12-21 13:34:36 -0700

    update RDMA experiments

--- RDMA_PINGPONG/implementation/server.c
+++ RDMA_PINGPONG/implementation/server.c
@@ -14,19 +14,18 @@ static struct context *s_ctx = NULL;
 struct l2fwd_port_statistics {
 	uint64_t tx;
 	uint64_t rx;
-	uint64_t dropped;
 	uint64_t rx_bytes;
 };
 
 static int on_event(struct rdma_cm_event *event);
 static void on_completion(struct ibv_wc *wc);
 
-
 #include "server_init.h"
 #include "server_action.h"
 
 static int on_event(struct rdma_cm_event *event);
 
+struct l2fwd_port_statistics port_statistics;
 
 int main(int argc, char **argv){
 
@@ -54,7 +53,12 @@ int main(int argc, char **argv){
 
   port = ntohs(rdma_get_src_port(listener));
 
+  memset(&port_statistics, 0, sizeof(port_statistics));
+
   printf("listening on port %d.\n", port);
+
+  printf("data %d %d \n", port_statistics->rx, port_statistics->tx);
+
 
   while (rdma_get_cm_event(ec, &event) == 0) {
     struct rdma_cm_event event_copy;
@@ -65,12 +69,10 @@ int main(int argc, char **argv){
     if (on_event(&event_copy)){
         break;
     }
-
   }
 
   rdma_destroy_id(listener);
   rdma_destroy_event_channel(ec);
-
   return 0;
 }
 
