55f9b9c140d67827e24d26654ee554d6e89873e6
SungHoHong2 <maverickjin88@gmail.com>
2017-12-21 17:53:34 -0700

    update RDMA experiments

--- RDMA_PINGPONG/implementation/server.c
+++ RDMA_PINGPONG/implementation/server.c
@@ -10,7 +10,7 @@
 
 
 const int TIMEOUT_IN_MS = 500; /* ms */
-const int BUFFER_SIZE = 100000;
+const int BUFFER_SIZE = 500000;
 static struct context *s_ctx = NULL;
 struct rdma_cm_event *event = NULL;
 
