d279f29131fabff38621b7f8a4da341674e8e01a
SungHoHong2 <maverickjin88@gmail.com>
2017-12-21 17:18:48 -0700

    update RDMA experiments

--- RDMA_PINGPONG/implementation/server_action.h
+++ RDMA_PINGPONG/implementation/server_action.h
@@ -80,8 +80,8 @@ int on_connection(void *context)
 
   if(conn->recv_region){
     // printf("checking the recv region file %ld \n\n", strlen(conn->recv_region));
-    conn->recv_region = conn->send_region;
-
+    memset(conn->send_region, '*', BUFFER_SIZE);
+    
     memset(&wr, 0, sizeof(wr));
     wr.opcode = IBV_WR_SEND;
     wr.sg_list = &sge;
