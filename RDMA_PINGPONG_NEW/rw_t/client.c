8c12a8775619c47635c7f7308f41aa0baa4a65bc
SungHoHong2 <maverickjin88@gmail.com>
2018-02-26 20:59:08 -0700

    update RDMA experiments

--- RDMA_PINGPONG_NEW/rw_t/client.c
+++ RDMA_PINGPONG_NEW/rw_t/client.c
@@ -174,8 +174,6 @@ int main(int argc, char   *argv[ ])
 	buf[0] = htonl(buf[0]);
 	buf[1] = htonl(buf[1]);
 
-  while (1) {
-
 	sge.addr 					  = (uintptr_t) buf;
 	sge.length                    = sizeof (uint32_t);
 	sge.lkey                      = mr->lkey;
@@ -199,12 +197,12 @@ int main(int argc, char   *argv[ ])
 	send_wr.sg_list               =&sge;
 	send_wr.num_sge               = 1;
 
-  while(i<3){
 	if (ibv_post_send(cm_id->qp, &send_wr,&bad_send_wr))
 		return 1;
 
 	/* 等待接收完成 */
 
+	while (i<3) {
 		if (ibv_get_cq_event(comp_chan,&evt_cq, &cq_context))
 			return 1;
 
