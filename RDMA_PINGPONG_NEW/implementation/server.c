89f03a827ad9492172edce6cd69ef27e38b9dd67
SungHoHong2 <maverickjin88@gmail.com>
2018-02-28 01:08:12 -0700

    update RDMA experiments

--- RDMA_PINGPONG_NEW/implementation/server.c
+++ RDMA_PINGPONG_NEW/implementation/server.c
@@ -73,7 +73,7 @@ static void on_pre_conn(struct rdma_cm_id *id)
 
   // frisk
   // some how this is letting you do the thing
-  posix_memalign((void **)&ctx->msg->buffer, sysconf(_SC_PAGESIZE), BUFFER_SIZE);
+  // posix_memalign((void **)&ctx->msg->buffer, sysconf(_SC_PAGESIZE), BUFFER_SIZE);
   // TEST_Z(ctx->msg->buffer_mr = ibv_reg_mr(rc_get_pd(), ctx->msg->buffer, BUFFER_SIZE, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE));
 
   post_receive(id);
