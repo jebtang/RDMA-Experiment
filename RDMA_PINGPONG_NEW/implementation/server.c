30487747828d2852713d5263643bfa39217c1cb9
SungHoHong2 <maverickjin88@gmail.com>
2018-02-28 01:06:47 -0700

    update RDMA experiments

--- RDMA_PINGPONG_NEW/implementation/server.c
+++ RDMA_PINGPONG_NEW/implementation/server.c
@@ -71,13 +71,10 @@ static void on_pre_conn(struct rdma_cm_id *id)
   posix_memalign((void **)&ctx->msg, sysconf(_SC_PAGESIZE), sizeof(*ctx->msg));
   TEST_Z(ctx->msg_mr = ibv_reg_mr(rc_get_pd(), ctx->msg, sizeof(*ctx->msg), IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE));
 
-  posix_memalign((void **)&ctx->msg->buffer, sysconf(_SC_PAGESIZE), BUFFER_SIZE);
-  TEST_Z(ctx->msg->buffer_mr = ibv_reg_mr(rc_get_pd(), ctx->msg->buffer, BUFFER_SIZE, IBV_ACCESS_LOCAL_WRITE));
-
   // frisk
   // some how this is letting you do the thing
-  // posix_memalign((void **)&ctx->msg->buffer, sysconf(_SC_PAGESIZE), BUFFER_SIZE);
-  // TEST_Z(ctx->buffer_mr = ibv_reg_mr(rc_get_pd(), ctx->msg->buffer, BUFFER_SIZE, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE));
+  posix_memalign((void **)&ctx->msg->buffer, sysconf(_SC_PAGESIZE), BUFFER_SIZE);
+  TEST_Z(ctx->msg->buffer_mr = ibv_reg_mr(rc_get_pd(), ctx->msg->buffer, BUFFER_SIZE, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE));
 
   post_receive(id);
 }
