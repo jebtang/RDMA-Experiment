87a0464555f4f4eac59dcbfef57976e24bee0279
SungHoHong2 <maverickjin88@gmail.com>
2018-02-28 01:37:36 -0700

    update RDMA experiments

--- RDMA_PINGPONG_NEW/implementation/server.c
+++ RDMA_PINGPONG_NEW/implementation/server.c
@@ -27,7 +27,7 @@ static void send_message(struct rdma_cm_id *id)
 
   memset(&wr, 0, sizeof(wr));
   //ctx->msg->buffer
-  strcpy(ctx->buffer, "howdy");
+  strcpy(ctx->msg->buffer, "howdy");
   // frisk problem here
 
   wr.wr_id = (uintptr_t)id;
