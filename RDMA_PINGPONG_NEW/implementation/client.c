25b0a102699c13c1567ac97cecf0341c844f645e
SungHoHong2 <maverickjin88@gmail.com>
2018-02-28 01:36:56 -0700

    update RDMA experiments

--- RDMA_PINGPONG_NEW/implementation/client.c
+++ RDMA_PINGPONG_NEW/implementation/client.c
@@ -71,7 +71,7 @@ static void on_pre_conn(struct rdma_cm_id *id)
   posix_memalign((void **)&ctx->msg, sysconf(_SC_PAGESIZE), sizeof(*ctx->msg));
   TEST_Z(ctx->msg_mr = ibv_reg_mr(rc_get_pd(), ctx->msg, sizeof(*ctx->msg), IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE));
   posix_memalign((void **)&ctx->msg->buffer, sysconf(_SC_PAGESIZE), buffer_size);
-  TEST_Z(ctx->msg->buffer_mr = ibv_reg_mr(rc_get_pd(), ctx->msg->buffer, buffer_size, IBV_ACCESS_LOCAL_WRITE));
+  TEST_Z(ctx->msg->buffer_mr = ibv_reg_mr(rc_get_pd(), ctx->msg->buffer, buffer_size, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE));
   post_receive(id);
 }
 
