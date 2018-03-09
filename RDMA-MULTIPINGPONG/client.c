858aeb2e0254f4be972483ed90916b041d352186
SungHoHong2 <maverickjin88@gmail.com>
2018-03-08 21:37:49 -0700

    update RDMA experiments

--- RDMA-MULTIPINGPONG/client.c
+++ RDMA-MULTIPINGPONG/client.c
@@ -11,7 +11,8 @@
 
 const char *DEFAULT_PORT = "12345";
 // const size_t BUFFER_SIZE = 64;
-const int LIMIT = 10000000; // 1000000;
+const int LIMIT = 1000000; // 1000000;
+                //10000000
 double total_throughput = 0;
 uint64_t start_time, end_time;
 
